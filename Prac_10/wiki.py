c995d1302d7d4e7dc165939759eb94db6649c029
rhysJD <69225713+rhysJD@users.noreply.github.com>
2020-11-05 19:50:17 +1000

    add wiki complete

---
+++ Prac_10/wiki.py
@@ -0,0 +1,15 @@
+"""
+CP1404 Practical 10
+Rhys Donaldson 13581558
+5/11/2020
+"""
+
+import wikipedia
+choice = input("What would you like to search for?\n>>>")
+while choice is not "":
+    try:
+        choice = wikipedia.page(choice)
+        print(choice.title + "\n" + choice.summary + "\n" + choice.url)
+        choice = input("What would you like to search for?\n>>>")
+    except wikipedia.exceptions.DisambiguationError:
+        choice = input("That page is a disambiguation. Please pick a different choice \n>>>")
